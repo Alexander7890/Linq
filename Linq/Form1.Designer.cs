f24c19a7d9023b641bdfc1dcc5d0283b04d25f42
aleksandrkostin7890 <aleksandrkostin7890@gmail.com>
2024-05-22 23:22:41 +0300

    Нужно добавить баттон которій будет взаимодействовать с файлами сохраниние уже есть нужно добавить чтение и вывод

--- Linq/Form1.Designer.cs
+++ Linq/Form1.Designer.cs
@@ -26,107 +26,108 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        ///    
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.TextBox textBoxCategory;
-        private System.Windows.Forms.TextBox textBoxSearch;
-        private System.Windows.Forms.ComboBox comboBoxSortBy;
-        private System.Windows.Forms.Button buttonFilter;
-        private System.Windows.Forms.Button buttonSort;
-        private System.Windows.Forms.Button buttonSearch;
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
+            this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.textBoxCategory = new System.Windows.Forms.TextBox();
-            this.textBoxSearch = new System.Windows.Forms.TextBox();
-            this.comboBoxSortBy = new System.Windows.Forms.ComboBox();
-            this.buttonFilter = new System.Windows.Forms.Button();
-            this.buttonSort = new System.Windows.Forms.Button();
-            this.buttonSearch = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(424, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(776, 290);
-            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.Size = new System.Drawing.Size(333, 246);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(438, 238);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(438, 294);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(438, 356);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(424, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(424, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
-            // 
-            // textBoxCategory
-            // 
-            this.textBoxCategory.Location = new System.Drawing.Point(12, 319);
-            this.textBoxCategory.Name = "textBoxCategory";
-            this.textBoxCategory.Size = new System.Drawing.Size(100, 20);
-            this.textBoxCategory.TabIndex = 1;
-
-            // 
-            // textBoxSearch
-            // 
-            this.textBoxSearch.Location = new System.Drawing.Point(12, 345);
-            this.textBoxSearch.Name = "textBoxSearch";
-            this.textBoxSearch.Size = new System.Drawing.Size(100, 20);
-            this.textBoxSearch.TabIndex = 2;
-
-            // 
-            // comboBoxSortBy
-            // 
-            this.comboBoxSortBy.FormattingEnabled = true;
-            this.comboBoxSortBy.Items.AddRange(new object[] {"Name","Price"});
-            this.comboBoxSortBy.Location = new System.Drawing.Point(12, 371);
-            this.comboBoxSortBy.Name = "comboBoxSortBy";
-            this.comboBoxSortBy.Size = new System.Drawing.Size(121, 21);
-            this.comboBoxSortBy.TabIndex = 3;
-
-            // 
-            // buttonFilter
-            // 
-            this.buttonFilter.Location = new System.Drawing.Point(139, 317);
-            this.buttonFilter.Name = "buttonFilter";
-            this.buttonFilter.Size = new System.Drawing.Size(75, 23);
-            this.buttonFilter.TabIndex = 4;
-            this.buttonFilter.Text = "Filter";
-            this.buttonFilter.UseVisualStyleBackColor = true;
-            this.buttonFilter.Click += new System.EventHandler(this.buttonFilter_Click);
-
-            // 
-            // buttonSort
-            // 
-            this.buttonSort.Location = new System.Drawing.Point(139, 369);
-            this.buttonSort.Name = "buttonSort";
-            this.buttonSort.Size = new System.Drawing.Size(75, 23);
-            this.buttonSort.TabIndex = 5;
-            this.buttonSort.Text = "Sort";
-            this.buttonSort.UseVisualStyleBackColor = true;
-            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
-
-            // 
-            // buttonSearch
-            // 
-            this.buttonSearch.Location = new System.Drawing.Point(139, 343);
-            this.buttonSearch.Name = "buttonSearch";
-            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
-            this.buttonSearch.TabIndex = 6;
-            this.buttonSearch.Text = "Search";
-            this.buttonSearch.UseVisualStyleBackColor = true;
-            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
-
--- Linq/Form1.cs
+++ Linq/Form1.cs
@@ -14,23 +14,36 @@ namespace Linq
     public partial class Form1 : Form
     {
         private List<Product> products;
+
         public Form1()
         {
             InitializeComponent();
-            products = new List<Product>();
+            products = new List<Product>
+        {
+            new Product(1, "Laptop", "Electronics", 999.99m, 10),
+            new Product(2, "Smartphone", "Electronics", 499.99m, 15),
+            new Product(3, "Tablet", "Electronics", 299.99m, 20),
+            new Product(4, "Headphones", "Accessories", 199.99m, 30),
+            new Product(5, "Charger", "Accessories", 49.99m, 50),
+            new Product(6, "Shoes", "Apparel", 79.99m, 40),
+            new Product(7, "T-shirt", "Apparel", 19.99m, 100)
+        };
+            SaveData(); // Save initial data to file
             LoadData();
             DisplayProducts(products);
+            PopulateCategoryComboBox();
+            PopulateSortByComboBox();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
         private void LoadData()
         {
-            if (File.Exists("products.csv"))
+            if (File.Exists("products.txt"))
             {
-                var lines = File.ReadAllLines("products.csv");
+                products.Clear();
+                var lines = File.ReadAllLines("products.txt");
                 foreach (var line in lines)
                 {
                     var parts = line.Split(',');
@@ -49,7 +62,7 @@ namespace Linq
         private void SaveData()
         {
             var lines = products.Select(p => $"{p.ID},{p.Name},{p.Category},{p.Price},{p.Quantity}");
-            File.WriteAllLines("products.csv", lines);
+            File.WriteAllLines("products.txt", lines);
         }
 
         private void DisplayProducts(IEnumerable<Product> productsToDisplay)
@@ -57,50 +70,106 @@ namespace Linq
             dataGridView1.DataSource = productsToDisplay.ToList();
         }
 
+        private void PopulateCategoryComboBox()
+        {
+            var categories = products.Select(p => p.Category).Distinct().ToList();
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(categories.ToArray());
+        }
+
+        private void PopulateSortByComboBox()
+        {
+            comboBox2.Items.Clear();
+            comboBox2.Items.Add("Name");
+            comboBox2.Items.Add("Price");
+        }
+
         private void FilterProducts(string category)
         {
-            var filteredProducts = products.Where(p => p.Category == category);
+            var filteredProducts = from p in products
+                                   where p.Category == category
+                                   select p;
             DisplayProducts(filteredProducts);
         }
 
         private void SortProducts(string sortBy)
         {
-            IEnumerable<Product> sortedProducts = sortBy switch
+            IEnumerable<Product> sortedProducts = null;
+            if (sortBy == "Name")
             {
-                "Name" => products.OrderBy(p => p.Name),
-                "Price" => products.OrderBy(p => p.Price),
-                _ => products
-            };
+                sortedProducts = from p in products
+                                 orderby p.Name
+                                 select p;
+            }
+            else if (sortBy == "Price")
+            {
+                sortedProducts = from p in products
+                                 orderby p.Price
+                                 select p;
+            }
             DisplayProducts(sortedProducts);
         }
 
         private void SearchProducts(string searchTerm)
         {
-            var searchedProducts = products.Where(p => p.Name.Contains(searchTerm, StringComparison.OrdinalIgnoreCase));
+            var searchedProducts = from p in products
+                                   where p.Name.StartsWith(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                                         p.Name.Equals(searchTerm, StringComparison.OrdinalIgnoreCase)
+                                   select p;
             DisplayProducts(searchedProducts);
         }
 
         private void buttonFilter_Click(object sender, EventArgs e)
         {
-            string category = textBoxCategory.Text;
-            FilterProducts(category);
+            string category = comboBox1.SelectedItem?.ToString();
+            if (category != null)
+            {
+                FilterProducts(category);
+            }
         }
 
         private void buttonSort_Click(object sender, EventArgs e)
         {
-            string sortBy = comboBoxSortBy.SelectedItem.ToString();
-            SortProducts(sortBy);
+            string sortBy = comboBox2.SelectedItem?.ToString();
+            if (sortBy != null)
+            {
+                SortProducts(sortBy);
+            }
         }
 
         private void buttonSearch_Click(object sender, EventArgs e)
         {
-            string searchTerm = textBoxSearch.Text;
+            string searchTerm = textBox1.Text;
             SearchProducts(searchTerm);
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             SaveData();
+        }
+
+    private void button1_Click(object sender, EventArgs e)
+        {
+            string category = comboBox1.SelectedItem?.ToString();
+            if (category != null)
+            {
+                FilterProducts(category);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sortBy = comboBox2.SelectedItem?.ToString();
+            if (sortBy != null)
+            {
+                SortProducts(sortBy);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string searchTerm = textBox1.Text;
+            SearchProducts(searchTerm);
         }
     }
 }
